9ab5bf0cce070e4bbf64bda80bb503b656ee2cae
Birju Vachhani <birjuvachhani@gmail.com>
2026-05-31 12:09:46 -0700

    Added some comments in the examples

--- llvm/ubsan_count/test/test.cpp
+++ llvm/ubsan_count/test/test.cpp
@@ -1,17 +1,33 @@
+//! This is a playground test file for analzying the IR.
+
 #include <array>
 
 struct Rotation {
   double data[9];
+  // UBSAN instrumentation is added for signed arithmetic in this function.
+  // Specially for the `count` variable, the instrumentation seems redundant.
+  // But the ScalarEvolution cannot prove this since instrumentation branch
+  // also dominates the latch.
+  // Without instrumentation, this function is optimized using SIMD instructions
+  // or built-in memcpy.
+  // With instrumentation, the loop persists with checks in every loop.
   Rotation(const Rotation &arg) {
     int count = 9;
-    while (count--)
+    while (count--) {
       data[count] = arg.data[count];
+    }
   }
 };
 
-Rotation make(const Rotation &a) { return Rotation(a); }
+Rotation testRotation(const Rotation &a) { return Rotation(a); }
 
-std::array<double, 9> function(int b, std::array<double, 9> data) {
+// In this function, the UBSAN optimization pass cannot prove that
+// signed overflow never occurs. So the instrumented code stays even after
+// optimization passes.
+std::array<double, 9> testCopy(int b, std::array<double, 9> data) {
+  if (b > 9) {
+    return {};
+  }
   std::array<double, 9> output;
   while (b--) {
     output[b] = data[b];
